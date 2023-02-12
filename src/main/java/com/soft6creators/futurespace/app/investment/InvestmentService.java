7b26a04efbe3801dbbec34eb1948f15a2d8462ca
cryptospaceassets <124940857+cryptospaceassets@users.noreply.github.com>
2023-02-12 17:38:16 +0100

    Update InvestmentService.java

--- src/main/java/com/soft6creators/futurespace/app/investment/InvestmentService.java
+++ src/main/java/com/soft6creators/futurespace/app/investment/InvestmentService.java
@@ -1,6 +1,7 @@
 package com.soft6creators.futurespace.app.investment;
 
 import java.util.Optional;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -50,6 +51,10 @@ public class InvestmentService {
 		}
 		
 
+	}
+	
+	public List<Investment> getInvestments() {
+		return (List<Investment>) investmentRepository.findAll();
 	}
 
 	public Optional<Investment> getInvestmentByAccount(@PathVariable int accountId) {
